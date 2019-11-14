80b77d9cf4db0ddd7b23053b2f2db38a7013cde6
kamorrow <keene.morrow@gmail.com>
2019-11-14 11:04:54 -0800

    Establish project & script

---
+++ spatial_2019-11-14.R
@@ -0,0 +1,23 @@
+###----------
+# Data Carpentry: Gesopatial Analysis in R
+# UCSB Libraries
+# 2019-11-14
+###----------
+
+# library(dplyr)
+# library(tidyverse)
+# library(raster)
+# library(rgdal)
+# library(rasterVis)
+# library(remotes)
+# library(sf)
+
+mass <- 47.5
+age <- 122
+mass <- mass * 2.3
+age <- age - 20
+
+ab_vector <- c("a", "b")
+combine <-  c(ab_vector, "c", "d")
+
+series <- 1:10
